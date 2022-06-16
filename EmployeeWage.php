83ca04f73e94cf1f36cadd463686b8964c5531a0
Kruthika <kruthikagnayaka05@gmail.com>
2022-06-17 00:26:55 +0530

    uc1 is done

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,6 +1,18 @@
 <?php
 echo "........Welcome to Employee Wage Computation.........";
 
+function emp(){
+    $is_present = 1;
+    $is_absent = 0;
+    $random = 0;
+
+        if($random == $is_present){
+            echo "Employee is present";
+        }else{
+            echo "Employee is absent";
+        }
+}
+emp();
 
 
 
