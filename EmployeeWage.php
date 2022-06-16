056d852f964ee9e6d48f7348bc5a996887bc8e40
Kruthika <kruthikagnayaka05@gmail.com>
2022-06-17 00:55:00 +0530

    uc2 is done

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -5,13 +5,19 @@ function emp(){
     $is_present = 1;
     $is_absent = 0;
     $r= rand(0 , 1);
+    $working_hour = 8;
+    $per_hour = 20;
+    $daily_wage =0;
     
-        if($r == $is_present){
-            echo "Employee is present";
-        }else if($r == $is_absent){
-            echo "Employee is absent";
-          
-        }
+    if($r == $is_present){
+        $daily_wage = $working_hour * $per_hour;
+        echo "Employee is present";
+        echo " Dailywage = $daily_wage ";
+    }else if($r == $is_absent){
+        echo "Employee is absent";
+        $daily_wage =0;
+        echo " Dailywage = $daily_wage ";
+    }
         }
 emp();
 
