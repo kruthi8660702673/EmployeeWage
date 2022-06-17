fdb1adea3a1dae2c300d5ff5c358cedf0704d3b4
Kruthika <kruthikagnayaka05@gmail.com>
2022-06-17 10:08:58 +0530

    uc3 is done

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -3,21 +3,26 @@ echo "........Welcome to Employee Wage Computation......... \n";
 
 function emp(){
     $is_present = 1;
-    $is_absent = 0;
-    $r= rand(0 , 1);
+    $is_parttime =2;
+    $r= rand(0 , 2);
     $working_hour = 8;
     $per_hour = 20;
+    $parttime_hour = 4;
     $daily_wage =0;
     
     if($r == $is_present){
         $daily_wage = $working_hour * $per_hour;
-        echo "Employee is present";
+        echo "Employee is present \n";
         echo " Dailywage = $daily_wage ";
-    }else if($r == $is_absent){
+    }else if($r == $is_parttime){
+        echo "Employee is parttime \n";
+        $daily_wage = $parttime_hour * $per_hour;
+        echo " Dailywage = $daily_wage ";
+    }else{
         echo "Employee is absent";
-        $daily_wage =0;
-        echo " Dailywage = $daily_wage ";
+        $daily_wage = 0;
     }
+
         }
 emp();
 
