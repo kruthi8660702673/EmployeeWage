bce6f16a73e43a4ddb2192083e04d0d5b83b47f2
Kruthika <kruthikagnayaka05@gmail.com>
2022-06-17 22:39:44 +0530

    EmployeeWage switch cases

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -3,23 +3,27 @@ echo "........Welcome to Employee Wage Computation......... \n";
 
 function emp()
 {
-    $is_present = 1;
-    $is_parttime = 2;
-    $r = rand(0, 2);
+    $is_FullTime = 1;
+    $is_PartTime = 2;
+    $is_Absent = 0;
+    $random = rand(0, 2);
     $per_hour = 20;
     $daily_wage = 0;
     $working_hour = 0;
 
-    if ($r == $is_present) {
-        $working_hour = 8;
-        echo "Employee is present \n";
-        
-    } else if ($r == $is_parttime) {
-        $working_hour = 4;
-        echo "Employee is parttime \n";
-    } else {
-        echo "Employee is absent \n";
-        $daily_wage = 0;
+    switch ($random) {
+        case $is_FullTime:
+            $working_hour = 8;
+            echo "Employee is present \n";
+            break;
+        case $is_PartTime:
+            $working_hour = 4;
+            echo "Employee is parttime \n";
+            break;
+        case $is_Absent:
+            echo "Employee is absent \n";
+            $daily_wage = 0;
+            break;
     }
 
     $daily_wage = $working_hour * $per_hour;
