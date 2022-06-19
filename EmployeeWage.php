f71e84d49fca226454cba01996bd9ba29a780dfe
Kruthika <kruthikagnayaka05@gmail.com>
2022-06-19 11:37:34 +0530

    monthlywage done

--- EmployeeWage.php
+++ EmployeeWage.php
@@ -1,32 +1,37 @@
 <?php
 echo "........Welcome to Employee Wage Computation......... \n";
 
-function emp()
-{
+function emp(){
     $is_FullTime = 1;
     $is_PartTime = 2;
     $is_Absent = 0;
-    $random = rand(0, 2);
     $per_hour = 20;
-    $daily_wage = 0;
     $working_hour = 0;
+    $max_Working_Days = 20;
+    $working_Day = 0;
+    $total_Working_Hours =0;    
+    while ($working_Day <= $max_Working_Days) { 
+        $random = rand(0, 2);
+        echo "Day " . $working_Day . "\n";
 
     switch ($random) {
-        case $is_FullTime:
+        case 1:
             $working_hour = 8;
             echo "Employee is present \n";
             break;
-        case $is_PartTime:
+        case 2:
             $working_hour = 4;
             echo "Employee is parttime \n";
             break;
-        case $is_Absent:
+        case 0:
             echo "Employee is absent \n";
-            $daily_wage = 0;
+            $working_hour = 0;
             break;
     }
-
-    $daily_wage = $working_hour * $per_hour;
-    echo " Dailywage = $daily_wage ";
+    $working_Day++;
+    $total_Working_Hours = $total_Working_Hours + $working_hour;
+}
+    $montly_wage = $total_Working_Hours * $per_hour;
+    echo " Monthlywage = $montly_wage ";
 }
 emp();
